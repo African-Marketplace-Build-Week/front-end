658d48c141f149632be8a688878b9ee3c8558858
samplesn08 <samplesn08@gmail.com>
2021-03-01 18:26:51 -0500

    styled login component using styled-components

--- african-marketplace/src/components/Login.js
+++ african-marketplace/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axiosWithAuth from "../utils/axiosWithAuth";
 import { useHistory } from "react-router-dom";
+import styled from 'styled-components';
 
 function LoginForm({ Login, error }) {
     const [details, setDetails] = useState({email: '', password: ''});
@@ -22,22 +23,82 @@ function LoginForm({ Login, error }) {
             );
           });  
     }
+    const LogForm = styled.form`
+      display: block;
+      position: relative;
+      margin: 20px;
+      width: 40%;
+      &:after {
+        content: '';
+        display: block;
+        position: absolute;
+        top: -5px;
+        left: -5px;
+        right: -5px;
+        bottom: -5px;
+        z-index: 1;
+        background-image: linear-gradient(to bottom right, #ffce00, #fe4880);
+      }
+    `;
+    const FormInner = styled.form`
+      position: relative;
+      display: block;
+      z-index: 2;
+      background-color: white;
+      padding: 30px;
+    `;
+    const LoginH2 = styled.h2`
+      color: #fe4880;
+      font-size: 2rem;
+      font-weight: bold;
+      margin-bottom: 30px;
+    `;
+    const FormGroup = styled.div`
+      display: block;
+      width: 300px;
+      margin-bottom: 15px;
+    `;
+    const LoginInput = styled.input`
+      display: block;
+      width: 100%;
+      padding: 10px 15px;
+      background-color: #d8d7d7;
+      border-radius: 8px;
+      transition: .3s;
+      &:focus {
+        box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+      }
+    `;
+    const LoginSubmit = styled.input`
+      display: inline-block;
+      padding: 10px 15px;
+      border-radius: 8px;
+      background-image: linear-gradient(to right, #ffce00 50%, #ffce00 50%, #fe4880);
+      background-size: 200%;
+      background-position: 0%;
+      transition: .5s;
+      color: white;
+      font-weight: bold;
+      cursor: pointer;
+      &:hover {
+        background-position: 100% 0%;
+      }
+    `;
     return (
-        <form onSubmit={submitHandler}>
-            <div className='form-inner'>
-                <h2>Login</h2>
-                {/* ERRORS */}
-                <div className='form-group'>
+        <LogForm onSubmit={submitHandler}>
+            <FormInner>
+                <LoginH2>Login</LoginH2>
+                <FormGroup>
                     <label htmlFor='email'>Email:</label>
-                    <input type='email' name ='email' id='email' placeholder='Email Address' onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
-                </div>
-                <div className='form-group'>
+                    <LoginInput type='email' name ='email' id='email' placeholder='Email Address' onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+                </FormGroup>
+                <FormGroup>
                     <label htmlFor='password'>Password:</label>
-                    <input type='password' name ='password' id='password' placeholder='Password' onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
-                </div>
-                <input type='submit' value='Log In' />
-            </div>
-        </form>
+                    <LoginInput type='password' name ='password' id='password' placeholder='Password' onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+                </FormGroup>
+                <LoginSubmit type='submit' value='Log In' />
+            </FormInner>
+        </LogForm>
     )
 }
 
