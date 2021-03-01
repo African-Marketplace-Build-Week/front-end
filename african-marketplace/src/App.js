d3a6fa07f8e8f88eb960f8f3a41ab241579d9e0c
Jonathan1600 <70250506+Jonathan1600@users.noreply.github.com>
2021-03-01 14:48:25 -0700

    style sheet sign up created

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -2,18 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./styles/styles.css";
 import Signup from "./components/SingUp";
+import "./styles/signUpStyle.css";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Link to="/">Home</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/login">Log In</Link>
-        {/* <Route exact path="/" component={Home} /> */}
-        <Route path="/signup" component={Signup} />
-        {/* <Route path="/login" component={Login} /> */}
-      </Router>
+      <Link to="/">Home</Link>
+      <Link to="/signup">Sign Up</Link>
+      <Link to="/login">Log In</Link>
+      {/* <Route exact path="/" component={Home} /> */}
+      <Route path="/signup" component={Signup} />
+      {/* <Route path="/login" component={Login} /> */}
     </div>
   );
 }
