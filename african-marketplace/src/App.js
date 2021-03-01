d5bdee8f1809e7fac983d9fcbe1d0d24373640d1
samplesn08 <samplesn08@gmail.com>
2021-03-01 11:53:22 -0500

    set up Route scaffolding

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './styles/styles.css'
 
 function App() {
   return (
     <div className="App">
-      helloooo
+      <Router>
+        <Link to='/'>Home</Link>
+        <Link to='/signup'>Sign Up</Link>
+        <Link to='/login'>Log In</Link>
+        <Route path='/signup' component={Signup} />
+        <Route path='/login' component={Login} />
+      </Router>
     </div>
   );
 }
