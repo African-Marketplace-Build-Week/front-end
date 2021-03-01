de78f67399874d8ecc722adae52fe704f404e404
Jonathan1600 <70250506+Jonathan1600@users.noreply.github.com>
2021-03-01 15:56:48 -0700

    removed styles

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Signup from "./components/SignUp";
 import "./styles/styles.css";
-import "./styles/signUpStyle.css";
-import Home from './components/Home';
-
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Switch>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
