f7a5ffe120bdc96be290b829729a4fb91435aedf
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-03-02 09:30:17 -0600

    Making Store

--- african-marketplace/src/index.js
+++ african-marketplace/src/index.js
@@ -2,10 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./Reducers";
+
+//Store Here -------------------
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+);
+
 
 ReactDOM.render(
   <Router>
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
   </Router>,
   document.getElementById("root")
 );
