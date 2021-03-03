55e0bf9eafdd7a2b48d69ca41f4299aaf5233827
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-03-03 16:17:58 -0600

    preview button home link

--- african-marketplace/src/components/Dashboard.js
+++ african-marketplace/src/components/Dashboard.js
@@ -3,13 +3,16 @@ import { marketplaceArray } from '../data/Data'
 import MarketItemList from './MarketItemList'
 import NewItem from './NewItem'
 import '../styles/dashboard.css'
+import { useHistory, Link } from "react-router-dom";
 
 
 export default function Home(props) {
+  const { push } = useHistory();
     console.log(marketplaceArray)
     return (
       <div className='dashboard-container'>
-      <h1 className='title-h1'> <span className='hello'>Hello!</span> Welcome to the African Marketplace.</h1> <br />
+        <div className='nav-bar-dashboard'>
+      <h1 className='title-h1'> <span className='hello'>Hello!</span> Welcome to the African Marketplace.</h1>  <br /></div>
       <div className='cards-container'>
   <div className="market-container">
       {marketplaceArray.length > 0 && marketplaceArray.map((item) => <MarketItemList 
@@ -18,8 +21,13 @@ export default function Home(props) {
       </div>
       <div className='new-item-form'>
         <NewItem />
+        <div className='home-link'>
+        <Link className='homeBtn' to="/">Home</Link>
+        </div>
+      </div>
+      
       </div>
       </div>
-      </div>
+      
     );
   }
